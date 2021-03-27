860cc98801095128027c3b24ebd33fb3d20dd8a4
edelmutic <edelmutic@gmail.com>
2021-03-27 20:32:25 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Subject, interval } from 'rxjs'
+import { takeUntil } from 'rxjs/operators'
+
+import './App.css'
+
 
 function App() {
+  const [time, setTime] = useState(0)
+  const [timerOn, setTimerOn] = useState(false)
+
+  useEffect(() => {
+    const sub$ = new Subject()
+    interval(1000)
+      .pipe(takeUntil(sub$))
+      .subscribe(() => {
+        if (timerOn) { setTime(prev => prev + 1) }
+      })
+    return () => {
+      sub$.next()
+      sub$.complete()
+    }
+  }, [timerOn])
+
+  const start_stop = () => {
+    setTimerOn(prev => !prev)
+    if (timerOn) {
+      setTime(0)
+    }
+  }
+
+  const wait = () => {
+    if (time !== 0) {
+      setTimerOn(false)
+    }
+  }
+
+  const reset = () => {
+    setTime(0)
+    setTimerOn(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <span>{('0' + Math.floor((time / 3600) % 100)).slice(-2)}:</span>
+        <span>{('0' + Math.floor((time / 60) % 60)).slice(-2)}:</span>
+        <span>{('0' + Math.floor(time % 60)).slice(-2)}</span>
+      </div>
+      <div>
+        <button type="button" className="btn btn-light" onClick={start_stop}>Start/Stop</button>
+        <button type="button" className="btn btn-light" onDoubleClick={wait}>Wait</button>
+        <button type="button" className="btn btn-dark" onClick={reset} onCl>Reset</button>
+      </div>
+    </div >
+  )
 }
 
-export default App;
+export default App
