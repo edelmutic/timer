37fe5faa418a488aa059c2ba2258e0cb9575c8fb
edelmutic <edelmutic@gmail.com>
2021-04-10 09:41:44 +0300

    add rxjs

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { Subject, interval } from 'rxjs'
-import { takeUntil } from 'rxjs/operators'
+import React, { useState, useEffect } from 'react';
+import { Subject, interval } from 'rxjs';
+import { takeUntil, bufferTime } from 'rxjs/operators';
 
-import './App.css'
-
+import './App.css';
 
 function App() {
-  const [time, setTime] = useState(0)
-  const [timerOn, setTimerOn] = useState(false)
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
 
   useEffect(() => {
-    const sub$ = new Subject()
+    const sub$ = new Subject();
     interval(1000)
       .pipe(takeUntil(sub$))
       .subscribe(() => {
-        if (timerOn) { setTime(prev => prev + 1) }
-      })
+        if (timerOn) {
+          setTime((prev) => prev + 1);
+        }
+      });
     return () => {
-      sub$.next()
-      sub$.complete()
-    }
-  }, [timerOn])
+      sub$.next();
+      sub$.complete();
+    };
+  }, [timerOn]);
 
   const start_stop = () => {
-    setTimerOn(prev => !prev)
+    setTimerOn((prev) => !prev);
     if (timerOn) {
-      setTime(0)
+      setTime(0);
     }
-  }
+  };
 
   const wait = () => {
     if (time !== 0) {
-      setTimerOn(false)
+      setTimerOn(false);
     }
-  }
+  };
 
   const reset = () => {
-    setTime(0)
-    setTimerOn(true)
-  }
+    setTime(0);
+    setTimerOn(true);
+  };
 
   return (
     <div className="App">
@@ -48,12 +49,18 @@ function App() {
         <span>{('0' + Math.floor(time % 60)).slice(-2)}</span>
       </div>
       <div>
-        <button type="button" className="btn btn-light" onClick={start_stop}>Start/Stop</button>
-        <button type="button" className="btn btn-light" onDoubleClick={wait}>Wait</button>
-        <button type="button" className="btn btn-dark" onClick={reset} onCl>Reset</button>
+        <button type="button" className="btn btn-light" onClick={start_stop}>
+          Start/Stop
+        </button>
+        <button type="button" className="btn btn-light" onDoubleClick={wait}>
+          Wait
+        </button>
+        <button type="button" className="btn btn-dark" onClick={reset} onCl>
+          Reset
+        </button>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
